e21b846231acb60b7d614b575ea602702c7be6c2
Nishant <nishantpani95@gmail.com>
2022-10-21 20:38:54 -0500

    Initial commit for intermediate submission

--- browser.c
+++ browser.c
@@ -34,12 +34,24 @@ tab_list TABS[MAX_TABS];
 
 // return total number of tabs
 int get_num_tabs () {
-  return 1;
+  int i, count = 0;
+
+  for (i=1; i<MAX_TABS; i++) {
+    if (TABS[i].free == 0)
+      count++;
+  }
+  return count;
 }
 
 // get next free tab index
 int get_free_tab () {
-  return 1;
+  int i;
+
+  for (i=1; i<MAX_TABS; i++) {
+    if (TABS[i].free == 1)
+      break;
+  }
+  return i;
 }
 
 // init TABS data structure
@@ -77,6 +89,12 @@ void init_favorites (char *fname) {
 // Return 0 if ok, -1 otherwise
 // Really a util but I want you to do it :-)
 int non_block_pipe (int fd) {
+  int nFlags;
+  
+  if ((nFlags = fcntl(fd, F_GETFL, 0)) < 0)
+    return -1;
+  if ((fcntl(fd, F_SETFL, nFlags | O_NONBLOCK)) < 0)
+    return -1;
   return 0;
 }
 
@@ -87,6 +105,9 @@ int non_block_pipe (int fd) {
 // Checks if tab is bad and url violates constraints; if so, return.
 // Otherwise, send NEW_URI_ENTERED command to the tab on inbound pipe
 void handle_uri (char *uri, int tab_index) {
+  printf("3\n");
+  //hard_coded
+  write(comm[1].inbound[1], uri, strlen(uri));
 }
 
 
@@ -94,18 +115,21 @@ void handle_uri (char *uri, int tab_index) {
 // If everything checks out, a NEW_URI_ENTERED command is sent (see Hint)
 // Short function
 void uri_entered_cb (GtkWidget* entry, gpointer data) {
-  
+  printf("2\n");
   if(data == NULL) {	
     return;
   }
 
   // Get the tab (hint: wrapper.h)
-
+  int tab_id = query_tab_id_for_request(entry, data);
+  //printf("%d \n", tab_id);
 
   // Get the URL (hint: wrapper.h)
-
+  char *url = get_entered_uri(entry);
+  //printf("%s \n", url);
 
   // Hint: now you are ready to handle_the_uri
+  handle_uri(url, tab_id);
 
 }
   
@@ -115,29 +139,54 @@ void uri_entered_cb (GtkWidget* entry, gpointer data) {
 // Create new tab process with pipes
 // Long function
 void new_tab_created_cb (GtkButton *button, gpointer data) {
-  
+  printf("1\n");
   if (data == NULL) {
     return;
   }
-
+  
   // at tab limit?
-
+  int active_tabs = get_num_tabs();
+  if (active_tabs > MAX_TABS){
+    printf("%d %d \n", active_tabs, MAX_TABS);
+    exit(1);
+  }
 
   // Get a free tab
-
+  int free_tab_id = get_free_tab();
 
   // Create communication pipes for this tab
-  
+
+  if (pipe(comm[free_tab_id].inbound) == -1 || pipe(comm[free_tab_id].outbound) == -1) {
+    perror("pipe error\n");
+    exit(1);
+  }
 
   // Make the read ends non-blocking 
-  
+  non_block_pipe (comm[free_tab_id].inbound[0]);
+  non_block_pipe (comm[free_tab_id].outbound[0]);
+
+  //char buf[100];
+  //read(comm[1].inbound[0], buf, 4);
+  //printf("%s\n", buf);
   
   // fork and create new render tab
   // Note: render has different arguments now: tab_index, both pairs of pipe fd's
   // (inbound then outbound) -- this last argument will be 4 integers "a b c d"
   // Hint: stringify args
-
-  // Controller parent just does some TABS bookkeeping
+  pid_t child = fork();
+  if (child < 0) {
+    perror("fork error\n");
+  }
+  else if (child == 0) {
+    char pipe_str[20], tab_str[20];
+    sprintf (tab_str, "%d", free_tab_id);
+    sprintf (pipe_str, "%d %d %d %d", comm[free_tab_id].inbound[1], comm[free_tab_id].inbound[0], comm[free_tab_id].outbound[1], comm[free_tab_id].outbound[0]);
+    execl("./render", "render", tab_str, pipe_str, NULL);
+  }
+  else if (child > 0) {
+    // Controller parent just does some TABS bookkeeping
+  }
+  
 }
 
 // This is called when a favorite is selected for rendering in a tab
@@ -190,11 +239,11 @@ int run_control() {
     // From any tab:
     //    IS_FAV: add uri to favorite menu (Hint: see wrapper.h), and update .favorites
     //    TAB_IS_DEAD: tab has exited, what should you do?
-
+;
     // Loop across all pipes from VALID tabs -- starting from 0
     for (i=0; i<MAX_TABS; i++) {
       if (TABS[i].free) continue;
-      nRead = read(comm[i].outbound[0], &req, sizeof(req_t));
+      //nRead = read(comm[i].outbound[0], &req, sizeof(req_t));
 
       // Check that nRead returned something before handling cases
 
@@ -222,6 +271,7 @@ int main(int argc, char **argv)
   // init blacklist (see util.h), and favorites (write this, see above)
 
 
+  run_control();
   // Fork controller
   // Child creates a pipe for itself comm[0]
   // then calls run_control ()
